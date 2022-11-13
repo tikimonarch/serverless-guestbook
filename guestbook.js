01434dc24fe17ee15c0c5649a23d3248af197fab
tikimonarch <65004283+tikimonarch@users.noreply.github.com>
2022-11-13 23:04:36 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/3eeab12a-f4f1-4c01-b658-a123d1890e72/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
